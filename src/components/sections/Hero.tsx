d68db7306caf042407d126e7e0944dd4436e32d2
Gurpreet Singh <103951591+Igurprxxt@users.noreply.github.com>
2025-12-15 20:31:16 +0530

    updated code

--- src/components/sections/Hero.tsx
+++ src/components/sections/Hero.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
 import { ArrowRight, Plane } from "lucide-react";
-import heroImage from "@/assets/hero-dishes.jpg";
+import heroImage from "@/assets/dish.jpg";
 
 const Hero = () => {
   return (
