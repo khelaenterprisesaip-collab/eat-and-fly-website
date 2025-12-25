105f47a390998436f87777e768efcd53dffe1ce3
Gurpreet Singh <103951591+Igurprxxt@users.noreply.github.com>
2025-12-25 10:25:07 +0530

    updated

--- src/components/ui/footer.tsx
+++ src/components/ui/footer.tsx
@@ -1,6 +1,6 @@
 import { Plane, Mail, Phone, MapPin } from "lucide-react";
 import { Link } from "react-router-dom";
-import logo from "@/assets/logor.png";
+import logo from "@/assets/logoq.png";
 
 const Footer = () => {
   const currentYear = new Date().getFullYear();
